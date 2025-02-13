b84f3ec54db294dc34f7e9353bbb8b38479e6825
Matt Haefling <m.haefling@protonmail.com>
2025-02-13 09:58:06 -0700

    iternation1: poster images are displaying

--- src/App/App.js
+++ src/App/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import searchIcon from '../icons/search.png';
 
 // Example imports (for later):
-// import { useState, useEffect } from 'react';
-// import moviePosters from '../data/movie_posters';
-// import movieDetails from '../data/movie_details';
-// import MoviesContainer from '../MoviesContainer/MoviesContainer';
+import { useState, useEffect } from 'react';
+import moviePosters from '../data/movie_posters';
+import movieDetails from '../data/movie_details';
+import MoviesContainer from '../MoviesContainer/MoviesContainer';
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <header>
         <h1>rancid tomatillos</h1>
       </header>
+      <MoviesContainer posters={moviePosters} />
     </main>
   );
 }
