c5fb505aa5f052a6a9898d21e9fc6516632f199e
Matt Haefling <m.haefling@protonmail.com>
2025-02-14 12:35:37 -0700

    iteration2: vote counts are now working and have been moved into the movies container

--- src/App/App.js
+++ src/App/App.js
@@ -7,41 +7,18 @@ import moviePosters from '../data/movie_posters';
 import movieDetails from '../data/movie_details';
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
 
-
 function App() {
 
   const [movies, setMovies] = useState(moviePosters);
-
-  function increaseVote(id) {
-    const updatedMovies = movies.map(movie => {
-      if (movie.id === id) {
-        return { ...movie, vote_count: movie.vote_count + 1 }
-      } else {
-        return { ...movie, movie }
-      }
-    })
-    setMovies(updatedMovies)
-  }
-
-  function decreaseVote(id) {
-    const updatedMovies = movies.map(movie => {
-      if (movie.id === id) {
-        return { ...movie, vote_count: movie.vote_count - 1 }
-      } else {
-        return { ...movie, movie }
-      }
-    })
-    setMovies(updatedMovies)
-  }
 
   return (
     <main className='App'>
       <header>
         <h1>rancid tomatillos</h1>
       </header>
-      <MoviesContainer moviePosters={movies} increaseVote={increaseVote} decreaseVote={decreaseVote} />
+      <MoviesContainer moviePosters={movies} setMovies={setMovies} />
     </main>
   );
-}
+};
 
 export default App;
--- src/MoviesContainer/MoviesContainer.js
+++ src/MoviesContainer/MoviesContainer.js
@@ -1,7 +1,29 @@
 import './MoviesContainer.css';
 import MoviePoster from '../MoviePoster/MoviePoster'
 
-function Movies({moviePosters, increaseVote, decreaseVote}) {
+function Movies({moviePosters, setMovies}) {
+
+  function increaseVote(id) {
+    const updatedMovies = moviePosters.map(movie => {
+      if (movie.id === id) {
+        return { ...movie, vote_count: movie.vote_count + 1 }
+      } else {
+        return { ...movie, movie }
+      };
+    });
+    setMovies(updatedMovies)
+  };
+
+  function decreaseVote(id) {
+    const updatedMovies = moviePosters.map(movie => {
+      if (movie.id === id) {
+        return { ...movie, vote_count: movie.vote_count - 1 }
+      } else {
+        return { ...movie, movie }
+      };
+    });
+    setMovies(updatedMovies)
+  };
 
   const allPosters = moviePosters.map(poster => {
     return (
