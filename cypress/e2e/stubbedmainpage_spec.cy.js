69457b3a0b8abebc2b0df1aca065c07315925646
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-02-19 00:27:52 -0500

    Expanded on tests to have more detail.

--- cypress/e2e/stubbedmainpage_spec.cy.js
+++ cypress/e2e/stubbedmainpage_spec.cy.js
@@ -10,12 +10,22 @@ describe('Movie Poster spec', () => {
     
     cy.wait('@getMovies');
 
-    cy.get('h1', { timeout: 10000 }).should('exist');
-    cy.get('.MoviesContainer').should('exist');
-    cy.get('.MoviePoster').should('have.length', 4); // Ensure the correct number of posters are displayed
-    cy.get('.MoviePoster').first().find('.MoviePoster_thumbnail').should('exist');
-    cy.get('.MoviePoster').first().find('.MoviePoster_voteCount').should('exist');
-    cy.get('.MoviePoster').last().find('.MoviePoster_thumbnail').should('exist');
-    cy.get('.MoviePoster').last().find('.MoviePoster_voteCount').should('exist');
-  });
+    cy.get('h1').should('contain', 'rancid tomatillos')
+    
+    cy.get('.MoviePoster').should('have.length', 4);
+
+    cy.get('.MoviePoster').first()
+      .find('.MoviePoster_thumbnail')
+      .should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//qJ2tW6WMUDux911r6m7haRef0WH.jpg'); 
+    cy.get('.MoviePoster').first()
+      .find('.MoviePoster_voteCount')
+      .should('contain.text', '32544'); 
+
+    cy.get('.MoviePoster').last()
+      .find('.MoviePoster_thumbnail')
+      .should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg');
+    cy.get('.MoviePoster').last()
+      .find('.MoviePoster_voteCount')
+      .should('contain.text', '27642'); 
+     });
 });
